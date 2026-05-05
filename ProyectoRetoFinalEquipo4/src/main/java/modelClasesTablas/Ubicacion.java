6dcfa9aca79c6d1967b0672a67f356e397686a76
AlbgoncebDAM <agonzalezc2502@educantabria.es>
2026-05-05 17:58:11 +0200

    PROG-02 - Crear clases modelo actualizacion
    Descripción:
    Crear las clases Java que representan los datos principales: Material, Categoría, Ubicación, Usuario, Préstamo y cualquier clase adicional que indique el modelo.
    
    Para cerrar:
    
    Atributos coherentes con la base de datos.
    Constructores y getters/setters implementados.
    Código compila.
    Clases preparadas para usarse desde los DAO.

--- ProyectoRetoFinalEquipo4/src/main/java/modelClasesTablas/Ubicacion.java
+++ ProyectoRetoFinalEquipo4/src/main/java/modelClasesTablas/Ubicacion.java
@@ -9,30 +9,50 @@ package modelClasesTablas;
  * @author DAM126
  */
 public class Ubicacion {
-    private int id_categoria;
-    private String nombre;
+    private int id_ubicacion;
+    private String armario;
+    private String balda;
+    private String cajon;
     private String descripcion;
 
-    public Ubicacion(int id_categoria, String nombre, String descripcion) {
-        this.id_categoria = id_categoria;
-        this.nombre = nombre;
+    public Ubicacion(int id_ubicacion, String armario, String balda, String cajon, String descripcion) {
+        this.id_ubicacion = id_ubicacion;
+        this.armario = armario;
+        this.balda = balda;
+        this.cajon = cajon;
         this.descripcion = descripcion;
     }
 
-    public int getId_categoria() {
-        return id_categoria;
+    public int getId_ubicacion() {
+        return id_ubicacion;
     }
 
-    public void setId_categoria(int id_categoria) {
-        this.id_categoria = id_categoria;
+    public void setId_ubicacion(int id_ubicacion) {
+        this.id_ubicacion = id_ubicacion;
     }
 
-    public String getNombre() {
-        return nombre;
+    public String getArmario() {
+        return armario;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setArmario(String armario) {
+        this.armario = armario;
+    }
+
+    public String getBalda() {
+        return balda;
+    }
+
+    public void setBalda(String balda) {
+        this.balda = balda;
+    }
+
+    public String getCajon() {
+        return cajon;
+    }
+
+    public void setCajon(String cajon) {
+        this.cajon = cajon;
     }
 
     public String getDescripcion() {
@@ -45,8 +65,9 @@ public class Ubicacion {
 
     @Override
     public String toString() {
-        return "Ubicacion{" + "id_categoria=" + id_categoria + ", nombre=" + nombre + ", descripcion=" + descripcion + '}';
+        return "Ubicacion{" + "id_ubicacion=" + id_ubicacion + ", armario=" + armario + ", balda=" + balda + ", cajon=" + cajon + ", descripcion=" + descripcion + '}';
     }
+    
     
     
 }
